881a1b6d20243ab7d909ad169e16ab740121b4b1
Purple Caramel <124286350+bbycaramel@users.noreply.github.com>
2024-07-18 00:25:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st program
+print (9 ** 0.5 * 5)
+
+#2nd program
+print (99.9 > 9.98 and 1000 !=1000.1)
+
+#3rd program
+n1 = 1234
+n2 = 5678
+
+m1 = int(str(n1)[1:3])
+m2 = int(str(n2)[1:3])
+
+print(m1 + m2)
+
+#4th program
+a = 58.88
+b = 88.39
+print (int(a) == int((b + 0.0000000001) % int(b) * 100) or int(b) == int((a + 0.0000000001) % int(a) * 100))
